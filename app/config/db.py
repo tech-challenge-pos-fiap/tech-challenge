58fd0c47666cdc06704ea8f03121beb54a9a31ec
psmari <marips0202@gmail.com>
2025-05-17 22:07:46 -0300

    refactor: Refactor product model and db string generation

--- app/config/db.py
+++ app/config/db.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://tech-challenge:tech-challenge@postgres:5432/tech-challenge"
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=os.path.join('envs', 'postgres.env'))
+
+DATABASE_URL = f'postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/{os.getenv("POSTGRES_DATABASE")}'
 
 engine = create_engine(DATABASE_URL)
 
