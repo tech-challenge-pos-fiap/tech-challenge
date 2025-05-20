df245726d5febd7a0ffe493f8dc265cac49dabf0
Mateus Nunes <matz11@hotmail.com>
2025-05-20 11:03:26 -0300

    feat: listagem de produtos por categoria com ajustes pós-PR #6

--- app/application/ports/produto_port.py
+++
@@ -1,7 +0,0 @@
-# app/application/ports/produto_port.py
-from abc import ABC, abstractmethod
-
-class ProdutoRepositoryPort(ABC):
-    @abstractmethod
-    def listar_por_categoria(self, categoria: str, skip: int = 0, limit: int = 10):
-        pass
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from typing import Union
-
+from app.adapters.api.routes.product_routes import router as produto_router
+from app.adapters.api.routes.payment_routes import router as payment_routes
 from fastapi import FastAPI
-from app.adapters.api.routes.produto_routes import router as produto_router
 
 app = FastAPI()
 
+app.include_router(payment_routes)
+app.include_router(produto_router)
 
 @app.get("/")
 def read_root():
