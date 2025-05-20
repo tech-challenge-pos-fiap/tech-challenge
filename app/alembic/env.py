1f298171e3f80277ef6c848afa90099c12679bc0
joao_gama <joao.gama@ssys.com>
2025-05-20 18:49:28 -0300

    remove debug print

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -65,7 +65,6 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    print(config.get_section(config.config_ini_section, {}))
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
